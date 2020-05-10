0328027825453b1485d9d97d2037fdf37a9c07ca
Danny Rhee <dongyunrhee@gmail.com>
2020-05-10 16:11:23 -0700

    User register/login API

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,33 +1,27 @@
 const express = require("express");
+const router = express.Router();
 const { check, validationResult } = require("express-validator");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const config = require("config");
-const router = express.Router();
 
-const auth = require("../../middleware/auth");
 const User = require("../../models/User");
 
-// @route   GET api/auth
-// @desc    Get Authenticated User Data
-// @access  Private
-router.get("/", auth, async (req, res) => {
-  try {
-    let user = await User.findById(req.user.id).select("-password");
-
-    res.json(user);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server error");
-  }
+// @route   GET api/users
+// @desc    Home route
+// @access  Public
+router.get("/", (req, res) => {
+  res.send("Users API!");
 });
 
-// @route   POST api/auth
-// @desc    Login User
+// @route   POST api/users
+// @desc    Register User
 // @access  Public
 router.post(
   "/",
   [
+    check("firstName", "First Name is required").not().isEmpty(),
+    check("lastName", "Last Name is required").not().isEmpty(),
     check("email", "Invalid Email address").isEmail(),
     check("password", "Invalid Password").isLength({ min: 6 }),
   ],
@@ -38,26 +32,34 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const { email, password } = req.body;
+    const { firstName, lastName, email, password } = req.body;
 
     try {
       let user = await User.findOne({ email });
 
       // Check if there is User with given email address
-      if (!user) {
+      if (user) {
         return res
           .status(400)
-          .json({ errors: [{ msg: "Invalid Credentials!" }] });
+          .json({ errors: [{ msg: "User already exists, please try again" }] });
       }
 
-      // Compare password
-      const isMatch = await bcrypt.compare(password, user.password);
+      const newUser = {
+        firstName,
+        lastName,
+        email,
+        password,
+      };
 
-      if (!isMatch) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: "Invalid Credentials" }] });
-      }
+      // Encrypt Password
+      const salt = await bcrypt.genSaltSync(10);
+      newUser.password = await bcrypt.hashSync(password, salt);
+
+      // Create User
+      user = await new User(newUser);
+
+      // Save User
+      await user.save();
 
       const payload = {
         user: {
