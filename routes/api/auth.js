a8b38b68d10656317820f4658db5a72e91990302
Danny Rhee <dongyunrhee@gmail.com>
2020-05-10 16:06:37 -0700

    Auth Middleware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,24 @@
 const express = require("express");
-
+const { check, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const config = require("config");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Auth Routes");
+const User = require("../../models/User");
+
+// @route   GET api/auth
+// @desc    Get Authenticated User Data
+// @access  Private
+router.get("/", auth, async (req, res) => {
+  try {
+    let user = await User.findById(req.user.id).select("-password");
+
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
 });
 
 module.exports = router;
