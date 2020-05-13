95be4d8071b7ca7630440ef1017548b0164ec3c6
Danny Rhee <dongyunrhee@gmail.com>
2020-05-13 13:30:39 -0700

    Initial Redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+
 import "./App.css";
 
 function App() {
-  return <div className="App">App component</div>;
+  return (
+    <Provider store={store}>
+      <Fragment>
+        <div className="App">App component</div>
+      </Fragment>
+    </Provider>
+  );
 }
 
 export default App;
